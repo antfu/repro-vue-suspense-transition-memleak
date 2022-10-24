c8c7cad79a23dc52284e72233fdc2b2a8a3cf378
Anthony Fu <anthonyfu117@hotmail.com>
2022-10-24 18:31:55 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Foo from './foo.vue'
+import Bar from './bar.vue'
+
+const a = ref(false)
+const count = ref(0)
+
+const toggle = () => {
+  a.value = !a.value
+  count.value += 1
+}
+</script>
+
+<template>
+  <main>
+    <div>
+      <h1>Count {{ count }}</h1>
+      <button @click="toggle()">
+        Toggle
+      </button>
+      <div>Open the memory tab in DevTools, take a memory snapshot. Click `toggle` for a few times, take another snapshot and see the diff. You may search for the instance amount of `EffectScope` for easier comparsion.</div>
+    </div>
+    <Transition mode="out-in">
+      <Suspense>
+        <Component :is="a ? Foo : Bar" />
+        <template #fallback>
+          Loading...
+        </template>
+      </Suspense>
+    </Transition>
+  </main>
+</template>
